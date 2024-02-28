c246b73b952831e2f633a5b4881b8240a0de6790
db <daudtopsec@gmail.com>
2024-02-28 18:11:19 +0000

    new test commit

---
+++ ConnectExchange.ps1
@@ -0,0 +1,5 @@
+﻿if (-NOT (Get-Module -ListAvailable -Name ExchangeOnlineManagement)) {
+Install-Module -Name ExchangeOnlineManagement
+}
+Import-Module ExchangeOnlineManagement
+$admin = Read-Host "Exchange admin email or UPN"
